c650695758c55a73c0cafc86762ff5e6b7008ce3
Ceeylla-Favv <bertleypriscy@gmail.com>
2026-05-25 13:13:52 +0100

    feat(rbac& ingress): map bedrock-dev-view to kubernetes view clusterrole and expose ui service via alb ingress resource

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -17,3 +17,17 @@ output "vpc_id" {
 output "assets_bucket_name" {
   value = module.s3_lambda.bucket_name
 }
+
+output "dev_access_key_id" {
+  value = module.iam.dev_access_key_id
+}
+
+output "dev_secret_access_key" {
+  value     = module.iam.dev_secret_access_key
+  sensitive = true
+}
+
+output "dev_console_password" {
+  value     = module.iam.dev_console_password
+  sensitive = true
+}
