671f93f1125a28d2a165d3bb60b213b82f6caddf
Ceeylla-Favv <bertleypriscy@gmail.com>
2026-06-20 17:46:06 +0100

    chore: update backend and grading.json for new aws account

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "project-bedrock-tfstate-904233100204"
+    bucket = "project-bedrock-tfstate-977145922444"
     key    = "project-bedrock/terraform.tfstate"
     region = "us-east-1"
   }
